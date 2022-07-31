fed677938a843e990023a45f9e2509f74a214af9
Ethanlinyf <e.yflin@gmail.com>
2022-07-31 15:52:58 +1000

    testclass again

---
+++ testclass.py
@@ -0,0 +1,17 @@
+
+
+class Pokemon:
+    def __init__(self, name, age, level, health, attack, magic, power):
+        self.name = name
+        self.age = age
+        self.level = level
+        self.health = health
+        self.attack = attack
+        self.magic = magic
+        self.power = power
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
